6e4376f616a3f14059ef07a9783af588db0de7d6
Ryx <118130241+Ryx69@users.noreply.github.com>
2025-09-02 12:41:50 +0200

    Update profile-image.js

--- src/cards/profile-image.js
+++ src/cards/profile-image.js
@@ -2,18 +2,31 @@ const DiscordArtsError = require('../utils/error.utils');
 const fetchUserData = require('../utils/fetch.utils');
 const { genPng } = require('../utils/profile-image.output.utils');
 
-async function profileImage(userId, options) {
-  if (!options) options = {};
+async function profileImage(userId, options = {}) {
   if (!userId || typeof userId !== 'string')
     throw new DiscordArtsError(
       `TypeError: Invalid argument for profileImage()\nExpected string userId, got ${typeof userId === 'undefined' || !userId ? 'undefined' : typeof userId}`
     );
 
-  const data = await fetchUserData(userId);
+  // Extract custom data from options
+  const customData = {
+    username: options.username,
+    globalName: options.globalName,
+    discriminator: options.discriminator,
+    bot: options.bot,
+    verified: options.verified,
+    createdTimestamp: options.createdTimestamp,
+    avatarURL: options.avatarURL,
+    bannerURL: options.bannerURL,
+    badges: options.customBadges,
+    profileColors: options.borderColor,
+    avatarFrame: options.avatarFrame
+  };
+
+  const data = await fetchUserData(userId, customData);
 
   try {
     const buffer = await genPng(data, options);
-
     return buffer;
   } catch (error) {
     if (error.message.includes('source rejected')) {
